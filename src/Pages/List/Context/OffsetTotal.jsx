88eb13cbf05577245a6d89e38b93cb321abc41cf
Frankie Lu <frankie_lu@trendmicro.com>
2021-04-11 00:08:54 +0800

    feat: get new data by scroll

--- src/Pages/List/Context/OffsetTotal.jsx
+++ src/Pages/List/Context/OffsetTotal.jsx
@@ -3,17 +3,16 @@ import React from "react";
 // create default context
 export const OffsetTotalContext = React.createContext();
 
-export const detectScroll = () => {
-  const scroll = window.scrollY;
-  const currentHeight = window.innerHeight + scroll;
-  if (currentHeight === document.body.clientHeight) {
-    console.warn("get new");
-  }
-};
-
 // set provider
 export const OffsetTotalContextProvider = ({ children }) => {
   const [offsetTotal, setOffsetTotal] = React.useState(0);
+  const detectScroll = () => {
+    const scroll = window.scrollY;
+    const currentHeight = window.innerHeight + scroll;
+    if (currentHeight === document.body.clientHeight) {
+      setOffsetTotal(offsetTotal + 1);
+    }
+  };
   React.useEffect(() => {
     window.addEventListener("scroll", detectScroll);
     return () => window.removeEventListener("scroll", detectScroll);
