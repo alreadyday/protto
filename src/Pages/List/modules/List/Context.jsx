58fa712a72863f358bde55ee0324ec6fc1c491da
Frankie Lu <frankie_lu@trendmicro.com>
2021-04-11 00:23:24 +0800

    feat: set width and height of card

--- src/Pages/List/modules/List/Context.jsx
+++ src/Pages/List/modules/List/Context.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { OffsetTotalContext } from "../../Context/OffsetTotal";
-
 // create default context
 export const ListContext = React.createContext();
 
 // set provider
 export const ListContextProvider = ({ children }) => {
-  const [list, setList] = React.useState([1]);
+  const [list, setList] = React.useState([]);
 
   return (
     <ListContext.Provider
@@ -22,9 +20,7 @@ export const ListContextProvider = ({ children }) => {
 
 // get list impl
 export const listGet = function (offsetTotal) {
-  return fetch(
-    `https://api.opensea.io/api/v1/assets?offsetTotal=${offsetTotal}`
-  )
+  return fetch(`https://api.opensea.io/api/v1/assets?offset=${offsetTotal}`)
     .then((response) => response.json())
     .then(({ assets }) => assets);
 };
--- src/Pages/List/modules/List/index.jsx
+++ src/Pages/List/modules/List/index.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 import { OffsetTotalContext } from "../../Context/OffsetTotal";
 import { ListContext, listGet } from "./Context";
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  width: 100px;
+  height: 150px;
+`;
+
+const CardImg = styled.img`
+  width: 75px;
+  height: 75px;
+`;
 
 export default function ListModule() {
   const { list, setList } = React.useContext(ListContext);
@@ -10,10 +26,15 @@ export default function ListModule() {
     setList([...list, ...newList]);
   }, [offsetTotal]);
   return (
-    <div>
+    <CardWrapper>
       {list.map((value) => {
-        return <div>{value.id}</div>;
+        return (
+          <Card key={value.id}>
+            <CardImg src={value.image_url} />
+            {value.id}
+          </Card>
+        );
       })}
-    </div>
+    </CardWrapper>
   );
 }
